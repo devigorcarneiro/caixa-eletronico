b9cc1568f036163892ddcdb0f107d3a519a75a81
Igor Carneiro <borgescapital@gmail.com>
2026-03-04 18:41:07 -0300

    feat: estrutura do menu de opções

--- caixaEletronico.c
+++ caixaEletronico.c
@@ -3,7 +3,7 @@
 int main() {
     printf("***************************************\n");
     printf("Caixa - É por mais vidas transformadas.\n");
-    printf("***************************************\n");
+    printf("***************************************\n\n");
 
     int accountNumber;
     int agencyNumber;
@@ -15,16 +15,23 @@ int main() {
     printf("Número da conta: %d\n", accountNumber);
     printf("Qual o número da agência? Ex:(1234)\n");
     scanf("%d", &agencyNumber);
-    printf("Numero da conta: %d\nNúmero da agência: %d\n", accountNumber, agencyNumber);
+    printf("\nNúmero da conta: %d\nNúmero da agência: %d\n\n", accountNumber, agencyNumber);
     printf("E como devemos te chamar?");
     scanf("%s", name);
 
     // Limpa a tela
     printf("\033[H\033[J");
 
-    printf("Bem-vindo %s! Agora você pode realizar suas operações financeiras escolhendo as opções de nosso menu!\n", name);
+    printf("Bem-vindo %s! Agora você pode realizar suas operações financeiras escolhendo as opções de nosso menu!\n\n", name);
     printf("Agência: %d\n", agencyNumber);
     printf("Conta: %d\n", accountNumber);
-    printf("Saldo: R$%d\n", balance);
+    printf("Saldo: R$%d\n\n", balance);
+
+    printf("Menu de opções:\n");
+    printf("1. Sacar\n");
+    printf("2. Depositar\n");
+    printf("3. Transferir\n");
+    printf("4. Extrato\n");
+    printf("5. Sair\n");
 
 }
